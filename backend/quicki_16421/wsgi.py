f0e26fc76e00330fedf1cc39e6ce47ad7f87b2be
Crowdbotics <team@crowdbotics.com>
2020-04-29 22:46:22 +0000

    Initial commit.

---
+++ backend/quicki_16421/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for quicki_16421 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quicki_16421.settings')
+
+application = get_wsgi_application()
